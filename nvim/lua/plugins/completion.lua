96a23cce6640d5baf26ea8cc18f6d4be0296b8da
Rom Art <root@romart.dev>
2025-04-15 12:35:22 +0300

    nvim: tune autopair a bit

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -10,7 +10,22 @@ return {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
         config = function()
-          require("nvim-autopairs").setup {}
+          require("nvim-autopairs").setup {
+            disable_filetype = { "TelescopePrompt", "vim" },
+            fast_wrap = {
+              map = '<M-e>',
+              chars = { '{', '[', '(', '"', "'" },
+              pattern = [=[[%'%"%>%]%)%}%,]]=],
+              end_key = '$',
+              before_key = 'h',
+              after_key = 'l',
+              cursor_pos_before = true,
+              keys = 'qwertyuiopzxcvbnmasdfghjkl',
+              manual_position = true,
+              highlight = 'Search',
+              highlight_grey = 'Comment'
+            },
+          }
         end
       },
       {
